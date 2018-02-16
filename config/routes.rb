f91cf23d2f04b3a03703dcc2342ab49eeffdf7a1
Dijekarim <dijekarim@gmail.com>
2018-02-16 19:09:31 +0700

    add products CRUD and better adminlte integrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: "products#index"
   resources :products
+  devise_scope :user do
+    get '/signout', to: 'devise/sessions#destroy', as: :signout
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
