28aace3a28d96d5e8058f4d46206a79573cb3f45
Dijekarim <dijekarim@gmail.com>
2018-02-16 19:10:22 +0700

    devise for user authorizations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+["Acer", "Lenovo", "Samsung", "Apple", "Axioo"].each_with_index do |merk, idx|
+  Product.create(nama: "Laptop #{merk}", kode: "LP-00#{idx+1}")
+end
+
+["AOC", "Xiaomi", "Msi", "Asus", "Logitech"].each_with_index do |merk, idx|
+  Product.create(nama: "Monitor #{merk}", kode: "MT-00#{idx+1}")
+end
