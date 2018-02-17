d286ab4826505cf9be0a6ea17e8f2937577eeb12
Dijekarim <dijekarim@gmail.com>
2018-02-17 13:45:04 +0700

    add order by name

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all.page(params[:page]).per(params[:per])
+    @products = Product.all.order(:nama).page(params[:page]).per(params[:per])
   end
 
   # GET /products/1
@@ -28,7 +28,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.html { redirect_to products_path, notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
